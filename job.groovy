272a63527bf09fb9c84a5cde47f1ce6ae0421946
Nicolai Birkmose Oksen <nicolaioksen@gmail.com>
2016-08-25 15:49:18 +0200

    Added first groovy job file

---
+++ job.groovy
@@ -0,0 +1,23 @@
+job('build') {
+    description 'Build and test the app.'
+    scm {
+        git{
+            remote {
+               github 'NicolaiOksen/Adventure-Time' 
+               credentials("2f0ddb6f-f67f-4c0b-9a17-09455139185c")
+            }
+        }
+    }
+    wrappers {
+        pretestedIntegration("SQUASHED", "master", "origin")
+    }
+    publishers {
+        pretestedIntegration()
+    }
+    triggers {
+        githubPush()
+    }
+    steps {
+        gradle 'test'
+    }
+}
